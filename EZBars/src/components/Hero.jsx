9996fc54399db1c84634e2fe030a7cf9eb0bddad
DominicCortez <dominic.cortez09k@gmail.com>
2023-09-27 11:59:30 +0800

    login form done next signup

--- EZBars/src/components/Hero.jsx
+++ EZBars/src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             </h1>
             <p className='text-lg text-gray-900 py-4'>Sales and Inventory Management for Small Businesses</p>
             <form>
-            <button onClick={navigateSignup} className=' px-8  py-3 rounded-md bg-[#FE8C37] text-white font-bold'>Sign Up</button>{/*Signup button*/}
+            <button onClick={navigateSignup} className=' px-8  py-3 rounded-md bg-[#FE8C37] text-white font-bold shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1'>Sign Up</button>{/*Signup button*/}
             </form>
 
             </div>
