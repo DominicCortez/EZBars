ac76a11bc3098b1356d40f69a3960426d886cebe
DominicCortez <dominic.cortez09k@gmail.com>
2023-10-21 17:31:26 +0800

    Im doneeee

--- EZBars/src/components/Footer.jsx
+++ EZBars/src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className='w-full bg-white py-[50px]'>{/*Containter for Hero*/}
         <div className='md:max-w-[1480px] m-auto max-w-[600px]'>{/*Create column grid to seperate image from text*/}
-            <h1 className='text-center text-2xl font-bold text-[#536E96]'>Insert Something amazing here or someshit</h1>
-            <p className='text-center text-lg'>Glasses are really versatile. First, you can have glasses-wearing girls take them off and suddenly become beautiful, or have girls wearing glasses flashing those cute grins, or have girls stealing the protagonist's glasses and putting them on like, "Haha, got your glasses!"</p>
+            <h1 className='text-center text-2xl font-bold text-[#536E96]'>Scan, Thrive, EZBars: Simplifying Success!</h1>
+            <p className='text-center text-lg'>EZBars envisions a future where small businesses effortlessly manage inventory through intuitive barcode systems. We simplify complexities, allowing businesses to focus on growth. In our vision, the barcode becomes a symbol of simplicity, accuracy, and success. EZBars strives for a world where small businesses thrive unburdened by inventory concerns.</p>
         </div>
     </div>
   )
